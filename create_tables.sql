0ab0624daf1045d40521a6a1bb490aab32f186dd
Mark Burghart <m.a.burghart@gmail.com>
2017-08-06 14:32:41 -0500

    Added sql script to create tables in database

---
+++ create_tables.sql
@@ -0,0 +1,11 @@
+-- SQL file for creating several tables in dissertation database
+-- Tables to create include:
+--      -Conditions (historical)
+--      -Conditions (Concussion only)
+--      -Encounter (concussion only)
+--      -Results (concussion only)
+--      -Demographics (done)
+--      -notes
+
+CREATE TABLE concussion_dx as
+select 
