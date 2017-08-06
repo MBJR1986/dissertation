48c794fde8823bad716afb6da8aa50042112f93e
Mark Burghart <m.a.burghart@gmail.com>
2017-08-06 09:20:54 -0500

    added general description of HERON tables

--- data_processing_log_regr.r
+++ data_processing_log_regr.r
@@ -1,12 +1,20 @@
 # Dissertation Code
 #Data Processing Script for Logistic Regression model
 
+#load packages
+library(sqldf)
+
 #load data
 setwd("/Users/Rachel/Documents/mark/dissertation/data/Mark_dissertation_20170718/")
-master <- read.csv('Mark_dissertation_20170718-data.csv')
+master <- read.csv('Mark_dissertation_20170718-data.csv', stringsAsFactors = FALSE)
 
 #start separating master out into different tables
 #TODO: Person (demographics), Conditions, notes, results (similar to SJS_v15 structure)
+#   - really need to understand what is in each table I received first...
+
+
+
+
 
 ###########################
 ####    CONDITIONS      ###
