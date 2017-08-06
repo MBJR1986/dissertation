00f05071144633ee148e65d749bebadd25d26d64
Mark Burghart <m.a.burghart@gmail.com>
2017-08-05 21:11:52 -0500

    initiate repo

---
+++ data_processing_log_regr.r
@@ -0,0 +1,6 @@
+# Dissertation Code
+#Data Processing Script for Logistic Regression model
+
+#load data
+setwd("/Users/Rachel/Documents/mark/dissertation/data/Mark_dissertation_20170718/")
+master <- read.csv('Mark_dissertation_20170718-data.csv')
