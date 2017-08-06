ca9d64b95afc9770f4901baea2b6b17528effead
Mark Burghart <m.a.burghart@gmail.com>
2017-08-06 12:48:33 -0500

    updated processing script

--- data_processing_log_regr.r
+++ data_processing_log_regr.r
@@ -13,8 +13,28 @@ master <- read.csv('Mark_dissertation_20170718-data.csv', stringsAsFactors = FAL
 #   - really need to understand what is in each table I received first...
 
 
+# for logistic regression model, I might just be able to use the person table, as it contains
+#the first and last values, which is all I'm concerned with, right???
+#   - Yeah no, that won't work... but all demographic info is in this table.
 
-
+###############################
+### Concussion Population   ###
+###############################
+#generate list of patient-ids with a concussion, along with dates and encounter
+pt_list <- unique(sqldf("select patient_num
+                    ,encounter_num
+                    ,start_date
+                    ,variable
+                    ,variable_index
+                    ,modifier_label
+                 from master
+                 where modifier = 'DiagObs:PRIMARY_DX_YN'"))#1626 unique people...
+#adding visit location
+sites <- c("ARW SPORTS MEDICINE CL", "EMERGENCY", "EMH EMERGENCY", "ICC SPECCARE NEURO", "ICC SPN REHAB MED CL",
+           "ICC SPORTS MEDICINE CL", "KUCC OVPK PT", "UKP-FAIRWAY NEUROLOGY", "UKP-KCK FAM MED", "UKP-KCK PED AMB",
+           "UKP-KU FAM MED", "UKP-KU PED AMB", "UKP-KU PED NEUROLOGY", "UKP-KU REHAB MED CL", "UKP-KUMW REHAB MED CL",
+           "UKP-LCOA NEUROLOGY", "UKP-LWR SPORTS MED CL", "ZZ30F REHAB MEDICINE", "ZZARW SPORTS REHAB CL", 
+           "ZZUKP-PV PED URGENT CA")
 
 ###########################
 ####    CONDITIONS      ###
