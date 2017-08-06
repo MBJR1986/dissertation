a3a7ae67096e722d4ed74d08937f3465b56e7fff
Mark Burghart <m.a.burghart@gmail.com>
2017-08-05 21:51:45 -0500

    Data processing script for Logistic Regression Model

--- data_processing_log_regr.r
+++ data_processing_log_regr.r
@@ -4,3 +4,13 @@
 #load data
 setwd("/Users/Rachel/Documents/mark/dissertation/data/Mark_dissertation_20170718/")
 master <- read.csv('Mark_dissertation_20170718-data.csv')
+
+#start separating master out into different tables
+#TODO: Person (demographics), Conditions, notes, results (similar to SJS_v15 structure)
+
+###########################
+####    CONDITIONS      ###
+###########################
+# Include: PID, EID, modifier label = Primary Dx
+
+#
