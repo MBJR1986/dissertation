12ddd5a7056ae73003f69894ab4c7b5eb31fb404
Mark Burghart <m.a.burghart@gmail.com>
2017-08-29 21:23:41 -0500

    continuing to add variables for logistic regression

--- data_processing_log_regr.r
+++ data_processing_log_regr.r
@@ -5,6 +5,8 @@
 #create_tables.sql script.
 
 #This script creates or converts data into variables for regression analysis.
+# This script will also create the final training/testing dataframe PRIOR to data splitting.
+# -     i.e. one row per person, with all variables.
 
 #load packages
 library(lubridate)
@@ -28,14 +30,60 @@ concussion_dx$encounter_num <- as.factor(concussion_dx$encounter_num)
 concussion_dx$variable_index <- as.factor(concussion_dx$variable_index)
 concussion_dx$encounter_loc <- as.factor(concussion_dx$encounter_loc)
 
-#LOR
+###################
+####    LOR     ###
+###################
 #Determine length of a person's recovery, from  the difference between 1st visit and last visit 
 #(assuming last visit = return to normal functioning)
 
-#create variable for eval_dt
+#create variable for eval_date and dc_date
 min = aggregate(concussion_dx$start_date,by=list(concussion_dx$patient_num),min)
 max = aggregate(concussion_dx$start_date,by=list(concussion_dx$patient_num),max)
-#TODO make column names same before merging
-concussion_dx = merge(concussion_dx,min,by.x=1,by.y=1)
+concussion_dx = merge(concussion_dx,min,by.x=1,by.y=1) #merge min date (eval)
+concussion_dx = merge(concussion_dx,max,by.x=1,by.y=1) #merge max date (dc)
+colnames(concussion_dx) = c("patient_num", "encounter_num", "start_date", "variable_index", 
+                            "variable", "encounter_loc", "eval_date", "dc_date") #adjust column names
 
+#date difference between eval and dc to represent length of treatment/recovery in DAYS (unit)
+concussion_dx$LOR <- as.numeric(difftime(concussion_dx$dc_date, concussion_dx$eval_date, units = "days"))
+
+#####################
+# historical dxs    #
+#####################
+med_hx$code_label <- as.factor(med_hx$code_label)
+contrasts(med_hx$code_label) #see how glm function will convert to binaries
+
+#####################
+#    demographics   #
+#####################
+demo$vital_status <- as.factor(demo$vital_status)
+demo$sex <- as.factor(demo$sex)
+demo$language <- as.factor(demo$language)
+demo$race <- as.factor(demo$race)
+demo$marital_status <- as.factor(demo$marital_status)
+demo$religion <- as.factor(demo$religion)
+
+#################################################
+#   create log_reg_full dataset for analysis    #
+#################################################
+log_reg <- as.data.frame(sqldf("select distinct conc.patient_num
+                                        ,conc.LOR
+                                        ,med.code_label as historical_condition
+                                        ,demo.sex
+                                        ,demo.language
+                                        ,demo.race
+                                        ,demo.marital_status
+                                        ,demo.religion
+                                        ,enc.encounter_loc
+                                    from concussion_dx as conc
+                                    left outer join med_hx as med
+                                    on conc.patient_num = med.patient_num
+                                    left outer join demo 
+                                    on conc.patient_num = demo.patient_num
+                                    left outer join enc_loc as enc
+                                    on conc.patient_num = enc.patient_num
+                                    group by conc.patient_num
+                                    order by conc.patient_num"))
+library(ade4) #package converts historical_conditions to binary dummy variables
+hx_cond_dummy <-as.data.frame(model.matrix( ~ historical_condition - 1, data=log_reg_full )) #still need to join back to patient_num
 
