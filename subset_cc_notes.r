5ee3e55424f242eee1a9f1c64f39148d3edf5d7f
Mark Burghart <mark.burghart@cerner.com>
2017-10-08 12:56:55 -0500

    adjusted order of variables and rows

--- subset_cc_notes.r
+++ subset_cc_notes.r
@@ -8,12 +8,29 @@ dx <- read.csv('concussion_dx_cohort.csv', stringsAsFactors = FALSE)
 #load library
 library(sqldf)
 
-#plan is to subset all patient_nums with concussion clinic note, then inner join list with notes file
+#convert to factors
+notes$patient_num <- as.factor(notes$patient_num)
 
+#convert to dates
+notes$note_date <- as.Date(notes$note_date, "%m/%d/%y %H:%M")
+notes$conc_dx_date <- as.Date(notes$conc_dx_date, "%m/%d/%y %H:%M")
+
+#remove rows with @ sign only in tval field
+notes <- subset(notes, subset = (tval != '@'))
+
+#parse out notes that occur before concussion_dx date for each patient_num
+notes$counter <- notes$note_date - notes$conc_dx_date #create counter comparing difference of dates between dx and note
+notes <- subset(notes, subset = (notes$counter >= 0)) #populate if counter is positive, meaning occurs after dx!
+
+#order by patient_num, note_date
+notes <-notes[with(notes,order(patient_num,note_date)),]
+
+#plan is to subset all patient_nums with concussion clinic note, then inner join list with notes file
 cc_pts <- sqldf("select distinct patient_num from notes where tval LIKE '%ORTHOPEDICS%'")
 
 #SUBSET NOTES
-cc_notes <- sqldf("select x.* from notes as x
+cc_notes <- sqldf("select x.patient_num, x.encounter_num, x.note_date, x.conc_dx_date, x.code_label, x.variable, x.variable_index, x.tval
+                  from notes as x
                   inner join cc_pts as y
                   ON x.patient_num = y.patient_num")
 
